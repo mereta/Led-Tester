112c4bbcbdb3dacff857625c110127e42a88a509
Mereta D <Mereta D@Mereta>
2017-02-26 22:26:49 +0000

    Test

--- LedTester/tests/test.py
+++ LedTester/tests/test.py
@@ -1,5 +1,24 @@
-'''
-Created on 26 Feb 2017
+def turnOn(x1, x2, y1, y2, a2d):
+    for i in range(x1, y1):
+        for j in range(x2, y2):
+            a2d[i][j] = True
+        return
 
-@author: Mereta D
-'''
+def switch(x1, x2, y1, y2, a2d):
+        for i in range(x1, y1):
+            for j in range(x2, y2):
+
+                if a2d[i][j] == False:
+                    a2d[i][j] = True
+
+                if a2d[i][j] == True:
+                    a2d[i][j] = False
+        return
+
+N = 10
+a2d = [ [0]*N for _ in range(N) ]
+turnOn(1, 1, 7, 7, a2d)
+print(a2d)
+switch(2, 2, 6, 6, a2d)
+print(a2d)
+
